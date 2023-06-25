a0957ffef278073bf41a1e5a0962bfadf8fe6e53
sabrina <sabrinaahmednitu@gmail.com>
2023-06-25 16:26:17 +0600

    contact us complete

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,87 +1,86 @@
-import React from "react";
-import { Card, Col, Container, Row } from "react-bootstrap";
-import "./Contact.css";
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import './Contact.css';
 
 const Contact = () => {
-    return (
-        // < className="contact-banner">
-        //     <h2>Contact With Us</h2>
-        // </>
-        <>
-                 <div className="contact-body">
-            <h2 className="contact-banner pt-5">Contact With Us </h2>
-            <div className="card-group pt-3 ">
-                <div className="card">
-                    
-                    
+  return (
+    <>
+      <div className="contact-body ">
+        <h2 className="text-center pt-5">Contact With Us </h2>
 
-<Card className="info-box contact-card" style={{ width: '18rem' }}>
-         <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <div className="footer-icon">
-                                <i class="fas fa-map-marker-alt"></i>
-                               
-                            </div>
-        <Card.Subtitle className="mb-2 text-muted">Address</Card.Subtitle>
-        <Card.Text>
-                                    31 park street, 5th Avenue, Dhanmondy, Dhaka.
-                                    
-        </Card.Text>
-       </Card.Body>
-        </Card>
-
-
+        <div className=" allContact-Card container">
+          <div className="card">
+            <div className="card-container">
+              <div className="icon">
+                <h5>Location</h5>
+                <div className="footer-icon">
+                  <i class="fas fa-map-marker-alt"></i>
                 </div>
-                <div className="card">
-                
-        <Card className="info-box contact-card" style={{ width: '18rem' }}>
-         <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                 <div className="footer-icon">
-                                <i class="fas fa-phone"></i>
-                               
-                            </div>
-        <Card.Subtitle className="mb-2 text-muted">Phone</Card.Subtitle>
-        <Card.Text>
-        +31 666 888 0001 <br />
-                                    +31 666 888 0023
-                                   
-        </Card.Text>
-       </Card.Body>
-        </Card>
-
-
+              </div>
+              <div className="card-text">
+                <div>
+                  <div className="mb-2 text-muted">Office Address</div>
+                  <div>
+                    <p>31 park street, 5th Avenue, <br /> Dhanmondy, Dhaka.</p>
+                  </div>
                 </div>
-                <div className="card">
-                
-        <Card className="info-box contact-card" style={{ width: '18rem' }}>
-         <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <div className="footer-icon">
-                                 <i class="fas fa-envelope"></i> 
-                            </div>  
-        <Card.Subtitle className="mb-2 text-muted">Email</Card.Subtitle>
-        <Card.Text>
-       info@example.com <br />
-     info@example.com
-                                  
-        </Card.Text>
-       </Card.Body>
-        </Card>
-
-
-                </div>
+              </div>
             </div>
-            <div className="mt-5 text-center" >
-                <h5> <p>You can easily contact us at anytime through the above phone number <br /> or email id. <br /> </p></h5>
-               
+          </div>
+
+          <div className="card">
+            <div className="card-container">
+              <div className="icon">
+                <h5>Phone Numbers</h5>
+                <div className="footer-icon">
+                  <i class="fas fa-phone"></i>
+                </div>
+              </div>
+              <div className="card-text">
+                <div>
+                  <div className="mb-2 text-muted">Phone Numbers</div>
+                  <div>
+                    <p>+316668880001 <br />
+                    +316668880001</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card-container">
+              <div className="icon">
+                <h5>Email</h5>
+                <div className="footer-icon">
+                  <i class="fas fa-envelope"></i>
+                </div>
+              </div>
+              <div className="card-text">
+                <div>
+                  <div className="mb-2 text-muted">Email Address</div>
+                  <div>
+                    <p>swiz@gmail.com <br /> sabrinaahmednitu@gmail.com </p>
+                    <p></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="mt-5 text-center">
+          <h5>
+            <p>
+              You can easily contact us at anytime through the above Phone
+              Number <br /> or Email Id. <br />
+            </p>
+          </h5>
+        </div>
       </div>
-            </div> 
-        </>
-    );
+    </>
+  );
 };
 
 export default Contact;
-
-
-
